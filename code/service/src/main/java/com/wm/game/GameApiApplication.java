4ed4cca352202794ad6be62c7a5612b1907e5824
wilson <wilson@wilsondeMacBook-Pro.local>
2021-01-23 13:18:50 +0800

    接口项目初始化

---
+++ code/service/src/main/java/com/wm/game/GameApiApplication.java
@@ -0,0 +1,13 @@
+package com.wm.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameApiApplication.class, args);
+    }
+
+}
---
+++ code/service/src/test/java/com/wm/game/GameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wm.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
